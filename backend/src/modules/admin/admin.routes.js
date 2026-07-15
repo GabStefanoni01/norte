a5432b26987e20a833faecc9cff8340a7c5eb294
GabStefanoni01 <gabstefanoni01@users.noreply.github.com>
2026-07-15 22:20:49 +0000

    feat(policy): wire policy routes, add admin endpoint to trigger retroactive consent emails

--- backend/src/modules/admin/admin.routes.js
+++ backend/src/modules/admin/admin.routes.js
@@ -4,6 +4,7 @@ const authGuard = require('../../middlewares/authGuard');
 const adminGuard = require('../../middlewares/adminGuard');
 const { aiLimiter } = require('../../middlewares/rateLimiters');
 const { executarRotinaDeLembretes } = require('../plans/plans.reminders');
+const { enviarSolicitacoesRetroativas } = require('../policy/policy.service');
 
 const router = Router();
 
@@ -14,6 +15,15 @@ router.patch('/users/:id/role', controller.atualizarRole);
 router.post('/lembretes/enviar', aiLimiter, async (req, res, next) => {
   try {
     const resultado = await executarRotinaDeLembretes();
+    res.json(resultado);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post('/politica/enviar-solicitacoes', aiLimiter, async (req, res, next) => {
+  try {
+    const resultado = await enviarSolicitacoesRetroativas();
     res.json(resultado);
   } catch (err) {
     next(err);
